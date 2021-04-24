05c3b3bef8da0e6ed78f863161f861f9d3d900fe
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-24 18:15:25 +0530

    Add files via upload

---
+++ Tasks of -DATA DISPLAY.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+TASKS- INTRO TO PYTHON
+"""
+#TASK 1
+print("Corona is also known as COVID-19.")
+print("It spreads from contact with the infected person.")
+print("Everyone except Nature,hates this pandemic. ")
+
+#TASK 2
+print("My School's name is Lokseva E school")
+print("I have been stuyding in it since I was in 1st !)
+print("We have a Huge playground.")
+print("I have a lot of friends there.")
+print("I love my School.")
+
+#TASK 3
+print("India is my country..lol")
+print("It is the seventh largest country in the world.")
+print("It has the 2nd largest road connections in the world.")
+print("It has 28 states and 9 union territories")
+print("I love my country and I am very proud to have it!")
